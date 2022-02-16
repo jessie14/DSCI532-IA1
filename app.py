ef58d355cb5c082f42f5d9b6fcf530f80ee70431
Jessie Wong <jessie14@student.ubc.ca>
2022-02-16 11:58:03 -0800

    Change to dropdown menu

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ from dash import Dash, html, Input, Output, dcc
 from gapminder import gapminder
 
 
-def plot_altair(xmax):
-    chart = alt.Chart(gapminder[gapminder['lifeExp'] < xmax]).mark_point().encode(
-        x='lifeExp',
-        y='pop',
-        tooltip='country').interactive()
-    return chart.to_html()
+
 
 app = Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
@@ -17,15 +12,22 @@ server = app.server
 app.layout = html.Div([
         html.Iframe(
             id='scatter',
-            srcDoc=plot_altair(xmax=0),
+            # srcDoc=plot_altair(xcol),
             style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Slider(id='xslider', min=25, max=100)])
+         dcc.Dropdown(
+            id='xcol-widget',
+            value='lifeExp',  # REQUIRED to show the plot on the first page load
+            options=[{'label': col, 'value': col} for col in gapminder.columns])])
         
 @app.callback(
     Output('scatter', 'srcDoc'),
-    Input('xslider', 'value'))
-def update_output(xmax):
-    return plot_altair(xmax)
+    Input('xcol-widget', 'value'))
+def plot_altair(xcol):
+    chart = alt.Chart(gapminder).mark_point().encode(
+        x=xcol,
+        y='pop',
+        tooltip='country').interactive()
+    return chart.to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
